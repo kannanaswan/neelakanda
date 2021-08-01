a5fe1da70e0ca475aab5d27373f6f93d583daa92
kannanaswan <88264541+kannanaswan@users.noreply.github.com>
2021-08-01 21:40:11 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = @Cybermediaworks,
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '🔰JOIN US🔰', url="https://t.me/Cybermediaworks"
                                 )
                         ]
                     ]
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('Developers', url='https://t.me/Cybermediaworks'),
+        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/kannanaswan/neelakanda')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/Cybermediaworks')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
